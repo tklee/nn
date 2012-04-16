d5d9fd67e2cbfea420b23e32f05f7c44053d56c1
ted <ted@ted-Lenovo-IdeaPad-Y550P.(none)>
2012-04-16 00:53:43 -0700

    none

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/', function(req, res){
   res.render('home', {
     story1: 'There\'s allegedly a scandal involving some Secret Service members. Dude, some people are living their life. Got it? Cool, how about you do your shitty work',
     story2: 'Some football star accompanied some 17 year old leukemia patient to her prom. That\'s quite nice of him, isn\'t it. You\'re directing attention to this? Do you have an infinite supply of attention? No.',
-    story3: 'Attacks subside in the Afghan capital. Does this affect your life? Nope.',
+    story3: 'Attacks subside in the Afghan capital. Does this affect your life? Nope. Really? Yup.',
     date: d2,
     month: monthname,
 //    details1: 
